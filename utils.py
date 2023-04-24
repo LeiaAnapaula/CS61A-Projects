7fa6d475e02e921c6f5b5069bcef7d74acdce456
Leia Anapaula Chavarria Davila <117881615+LeiaAnapaula@users.noreply.github.com>
2023-04-23 18:04:07 -0700

    Ants Project
    A tower defense game called Ants Vs. SomeBees. Your ants must protect their queen from the evil bees that invade your territory. Irritate the bees enough by throwing leaves at them, and they will be vanquished. Fail to pester the airborne intruders adequately, and your queen will succumb to the bees' wrath. PopCap Games' Plants Vs. inspire this game.

--- utils.py
+++ utils.py
@@ -1,132 +1,84 @@
-"Utility functions for file and string manipulation"
+def class_method_wrapper(method, pre=None, post=None):
+    """Given a class METHOD and two wrapper function, a PRE-function and
+    POST-function, first calls the pre-wrapper, calls the wrapped class method,
+    and then calls the post-wrapper.
 
-import string
-from math import sqrt
+    All wrappers should have the parameters (self, rv, *args). However,
+    pre-wrappers will always have `None` passed in as `rv`, since a return
+    value has not been evaluated yet.
 
-############################
-# String utility functions #
-############################
-
-
-def lines_from_file(path):
-    """Return a list of strings, one for each line in a file."""
-    with open(path, 'r') as f:
-        return [line.strip() for line in f.readlines()]
-
-
-def remove_punctuation(s):
-    """Return a string with the same contents as s, but with punctuation removed.
-
-    >>> remove_punctuation("It's a lovely day, don't you think?")
-    'Its a lovely day dont you think'
-    >>> remove_punctuation("Its a lovely day dont you think")
-    'Its a lovely day dont you think'
+    >>> def pre_wrapper(instance, rv, *args):
+    ...     print('Pre-wrapper called: {0}'.format(args))
+    >>> def post_wrapper(instance, rv, *args):
+    ...     print('Post-wrapper called: {0} -> {1}'.format(args, rv))
+    >>> class Foo:
+    ...     def __init__(self):
+    ...         self.bar = 20
+    ...     def method(self, var1, var2):
+    ...         print('Original method called')
+    ...         return var1 + var2 + self.bar
+    >>> Foo.method = class_method_wrapper(Foo.method, pre_wrapper, post_wrapper)
+    >>> f = Foo()
+    >>> x = f.method(1, 2)
+    Pre-wrapper called: (1, 2)
+    Original method called
+    Post-wrapper called: (1, 2) -> 23
+    >>> x
+    23
     """
-    punctuation_remover = str.maketrans('', '', string.punctuation)
-    return s.strip().translate(punctuation_remover)
+
+    def wrapped_method(self, *args):
+        pre(self, None, *args) if pre else None
+        rv = method(self, *args)
+        post(self, rv, *args) if post else None
+        return rv
+    return wrapped_method
 
 
-def lower(s):
-    """Return a lowercased version of s.
+def print_expired_insects(self, rv, *args):
+    """Post-wrapper for Insect.reduce_health, and will print a message if the
+    insect has expired (health reduced to 0).
 
-    >>> lower("HELLO")
-    'hello'
-    >>> lower("World")
-    'world'
-    >>> lower("hello WORLD")
-    'hello world'
+    >>> from ants import Insect, Bee, ThrowerAnt, Place
+    >>> Insect.reduce_health = class_method_wrapper(Insect.reduce_health,
+    ...         pre=print_expired_insects)
+    >>> place = Place('Test')
+    >>> bee = Bee(3)
+    >>> place.add_insect(bee)
+    >>> bee.reduce_health(2)
+    >>> bee.reduce_health(1)
+    Bee(Test) ran out of health and expired
+    >>> thrower = ThrowerAnt()
+    >>> place.add_insect(thrower)
+    >>> thrower.reduce_health(1)
+    ThrowerAnt(Test) ran out of health and expired
     """
-    return s.lower()
+    if self.health <= args[0]:
+        print('{0}({1}) ran out of health and expired'.format(
+            type(self).__name__, self.place))
 
 
-def split(s):
-    """Return a list of words contained in s, which are sequences of characters
-    separated by whitespace (spaces, tabs, etc.).
+def print_thrower_target(self, rv, *args):
+    """Prints the target of a ThrowerAnt, if the ThrowerAnt found a target.
 
-    >>> split("It's a lovely day, don't you think?")
-    ["It's", 'a', 'lovely', 'day,', "don't", 'you', 'think?']
+    >>> from ants import *
+    >>> beehive = Hive(AssaultPlan())
+    >>> dimensions = (1, 9)
+    >>> gamestate = GameState(None, beehive, ant_types(), dry_layout, dimensions)
+    >>> ThrowerAnt.nearest_bee = class_method_wrapper(ThrowerAnt.nearest_bee,
+    ...         post=print_thrower_target)
+    >>> thrower = ThrowerAnt()
+    >>> short = ShortThrower()
+    >>> bee = Bee(5)
+    >>> gamestate.places['tunnel_0_1'].add_insect(short)
+    >>> gamestate.places['tunnel_0_0'].add_insect(thrower)
+    >>> gamestate.places['tunnel_0_5'].add_insect(bee)
+    >>> thrower.action(gamestate)
+    ThrowerAnt(1, tunnel_0_0) targeted Bee(5, tunnel_0_5)
+    >>> short.action(gamestate)    # Bee not in range of ShortThrower
+    >>> bee.action(gamestate)      # Bee moves into range
+    >>> short.action(gamestate)
+    ShortThrower(1, tunnel_0_1) targeted Bee(4, tunnel_0_4)
     """
-    return s.split()
-
-#############################
-# Keyboard layout functions #
-#############################
-
-
-KEY_LAYOUT = [["1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "-", "="],
-              ["q", "w", "e", "r", "t", "y", "u", "i", "o", "p", "[", "]"],
-              ["a", "s", "d", "f", "g", "h", "j", "k", "l", ";", "'"],
-              ["z", "x", "c", "v", "b", "n", "m", ",", ".", "/"],
-              [" "]]
-
-
-def distance(p1, p2):
-    """Return the Euclidean distance between two points
-
-    The Euclidean distance between two points, (x1, y1) and (x2, y2)
-    is the square root of (x1 - x2) ** 2 + (y1 - y2) ** 2
-
-    >>> distance((0, 1), (1, 1))
-    1.0
-    >>> distance((1, 1), (1, 1))
-    0.0
-    >>> round(distance((4, 0), (0, 4)), 3)
-    5.657
-    """
-    return sqrt((p1[0] - p2[0])**2 + (p1[1] - p2[1])**2)
-
-
-def get_key_distances():
-    """Return a new dictionary mapping key pairs to distances.
-
-    Each key of the dictionary is a tuple of two
-    letters as strings, and each value is the euclidean distance
-    between the two letters on a standard QWERTY keyboard, normalized
-
-    The scaling is constant, so a pair of keys that are twice
-    as far have a distance value that is twice as great
-
-    >>> distances = get_key_distances()
-    >>> distances["a", "a"]
-    0.0
-    >>> round(distances["a", "d"],3)
-    1.367
-    >>> round(distances["d", "a"],3)
-    1.367
-    """
-    key_distance = {}
-
-    def compute_pairwise_distances(i, j, d):
-        for x in range(len(KEY_LAYOUT)):
-            for y in range(len(KEY_LAYOUT[x])):
-                l1 = KEY_LAYOUT[i][j]
-                l2 = KEY_LAYOUT[x][y]
-                d[l1, l2] = distance((i, j), (x, y))
-
-    for i in range(len(KEY_LAYOUT)):
-        for j in range(len(KEY_LAYOUT[i])):
-            compute_pairwise_distances(i, j, key_distance)
-
-    max_value = max(key_distance.values())
-    return {key: value * 8 / max_value for key, value in key_distance.items()}
-
-
-def count(f):
-    """Keeps track of the number of times a function f is called using the
-    variable call_count
-
-    >>> def factorial(n):
-    ...     if n <= 1:
-    ...         return 1
-    ...     return n * factorial(n - 1)
-    >>> factorial = count(factorial)
-    >>> factorial(5)
-    120
-    >>> factorial.call_count
-    5
-    """
-    def counted(*args):
-        counted.call_count += 1
-        return f(*args)
-    counted.call_count = 0
-    return counted
+    if rv is not None:
+        print('{0} targeted {1}'.format(self, rv))

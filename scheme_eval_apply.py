26581d2599fb0e5f47b14a06156214c308badde5
Leia Anapaula Chavarria Davila <117881615+LeiaAnapaula@users.noreply.github.com>
2023-04-23 18:08:37 -0700

    Scheme
    The Scheme project involves writing an interpreter for the Scheme language. A programming language like Python is useful because we can define an interpreter, a program that carries out Python's evaluation and execution procedures. It is no exaggeration to regard this as the most fundamental idea in programming, that an interpreter, which determines the meaning of expressions in a programming language, is just another program.

---
+++ scheme_eval_apply.py
@@ -0,0 +1,115 @@
+import sys
+
+from pair import *
+from scheme_utils import *
+from ucb import main, trace
+
+import scheme_forms
+
+##############
+# Eval/Apply #
+##############
+
+
+def scheme_eval(expr, env, _=None):  # Optional third argument is ignored
+    """Evaluate Scheme expression EXPR in Frame ENV.
+
+    >>> expr = read_line('(+ 2 2)')
+    >>> expr
+    Pair('+', Pair(2, Pair(2, nil))) 
+    >>> scheme_eval(expr, create_global_frame())
+    4
+    """
+    # Evaluate atoms ; the way how scheme read is through PAIRs
+    if scheme_symbolp(expr):
+        return env.lookup(expr)
+    elif self_evaluating(expr):
+        return expr
+
+    # All non-atomic expressions are lists (combinations)
+    if not scheme_listp(expr):
+        raise SchemeError('malformed list: {0}'.format(repl_str(expr)))
+    first, rest = expr.first, expr.rest
+    if scheme_symbolp(first) and first in scheme_forms.SPECIAL_FORMS:
+        return scheme_forms.SPECIAL_FORMS[first](rest, env)
+    else: # if the first is not a symbol then what? 1) keep iterating until find a symbol, or return error because everything in scheme is a list.
+        # BEGIN PROBLEM 3
+        "*** YOUR CODE HERE ***"
+        # END PROBLEM 3
+
+
+def scheme_apply(procedure, args, env):
+    """Apply Scheme PROCEDURE to argument values ARGS (a Scheme list) in
+    Frame ENV, the current environment."""
+    validate_procedure(procedure)
+    if not isinstance(env, Frame):
+       assert False, "Not a Frame: {}".format(env)
+    if isinstance(procedure, BuiltinProcedure):
+        # BEGIN PROBLEM 2
+        #assert True # or assert a correct number of arguments
+        arg_list = []
+        while args is not nil:
+            arg_list.append(args.first)
+            args = args.rest
+
+        if procedure.need_env:
+            arg_list.append(env) # "to this python list" it may be the args or first/rest
+        # END PROBLEM 2
+        try:
+            # BEGIN PROBLEM 2
+            #procedure(first) # apply procedure to the first element of the list, second, third and so on.
+            #procedure(rest)
+            
+            return procedure.py_func(*arg_list) # asterisk means all of the arguments :)
+            # END PROBLEM 2
+        except TypeError as err:
+            raise SchemeError('incorrect number of arguments: {0}'.format(procedure))
+    elif isinstance(procedure, LambdaProcedure):
+        # BEGIN PROBLEM 9
+        "*** YOUR CODE HERE ***"
+        # END PROBLEM 9
+    elif isinstance(procedure, MuProcedure):
+        # BEGIN PROBLEM 11
+        "*** YOUR CODE HERE ***"
+        # END PROBLEM 11
+    else:
+        assert False, "Unexpected procedure: {}".format(procedure)
+
+
+def eval_all(expressions, env):
+    """Evaluate each expression in the Scheme list EXPRESSIONS in
+    Frame ENV (the current environment) and return the value of the last.
+
+    >>> eval_all(read_line("(1)"), create_global_frame())
+    1
+    >>> eval_all(read_line("(1 2)"), create_global_frame())
+    2
+    >>> x = eval_all(read_line("((print 1) 2)"), create_global_frame())
+    1
+    >>> x
+    2
+    >>> eval_all(read_line("((define x 2) x)"), create_global_frame())
+    2
+    """
+    # BEGIN PROBLEM 6
+    return scheme_eval(expressions.first, env)  # replace this with lines of your own code
+    # END PROBLEM 6
+
+
+class Unevaluated:
+    """An expression and an environment in which it is to be evaluated."""
+
+    def __init__(self, expr, env):
+        """Expression EXPR to be evaluated in Frame ENV."""
+        self.expr = expr
+        self.env = env
+
+
+def complete_apply(procedure, args, env):
+    """Apply procedure to args in env; ensure the result is not an Unevaluated."""
+    validate_procedure(procedure)
+    val = scheme_apply(procedure, args, env)
+    if isinstance(val, Unevaluated):
+        return scheme_eval(val.expr, val.env)
+    else:
+        return val
